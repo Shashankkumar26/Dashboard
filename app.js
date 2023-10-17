31f2a0684307391c01adab0fdbad76d36a534cc2
Shashank Kumar <sk26kumar99@gmail.com>
2023-10-17 16:33:56 +0530

    update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const patients = [
     {
         name: "Jack Sparrow",
         age: 40,
+        image : "/public/photo5.jpg" ,
         heartRate: 75, // in beats per minute
         bodyTemperature: 37, // in Celsius
         glucose: 120, // in mg/dL
@@ -18,6 +19,7 @@ const patients = [
     {
         name: "Jane Smith",
         age: 28,
+        image : "/public/photo4.jpg" ,
         heartRate: 68,
         bodyTemperature: 36.5,
         glucose: 90,
@@ -34,6 +36,7 @@ const patients = [
     {
         name: "Robert Johnson",
         age: 55,
+        image : "/public/photo3.jpg" ,
         heartRate: 80,
         bodyTemperature: 36.9,
         glucose: 140,
@@ -50,6 +53,7 @@ const patients = [
     {
         name: "Sarah Wilson",
         age: 35,
+        image : "/public/photo2.jpg" ,
         heartRate: 70,
         bodyTemperature: 37.2,
         glucose: 110,
@@ -66,6 +70,7 @@ const patients = [
     {
         name: "Michael Brown",
         age: 50,
+        image : "/public/photo1.jpg" ,
         heartRate: 72,
         bodyTemperature: 36.8,
         glucose: 130,
@@ -101,7 +106,7 @@ function updatePatientData(patientIndex) {
     document.getElementById("patientWeight").textContent = currentPatient.information.weight;
     document.getElementById("patientHeart").textContent = currentPatient.heartRate;
     document.getElementById("patientTemp").textContent = currentPatient.bodyTemperature;
-    
+    document.getElementById("patientImage").src = currentPatient.image;
 }
 
 // Initialize with the first patient
